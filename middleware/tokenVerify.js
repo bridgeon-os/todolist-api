d5499395a44de17d7dc1a6c8d6d120fa642edfbf
codewithrabeeh <rabeehebrahim48@gmail.com>
2023-07-25 20:53:35 +0530

    testing todo read all

--- middleware/tokenVerify.js
+++ middleware/tokenVerify.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = {
     tokenVerify: (req, res, next) => {
+        console.log(req.body)
         const authHeader = req.headers.authorization
         if (!authHeader) {
             return res.status(401).json({ error: "Unauthrized ❗" })
