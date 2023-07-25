1163ccbd4c15481b061124adf2a4bfd7cdc795d0
codewithrabeeh <rabeehebrahim48@gmail.com>
2023-07-25 21:56:54 +0530

    fix: render.com cant process body

--- middleware/tokenVerify.js
+++ middleware/tokenVerify.js
@@ -12,6 +12,7 @@ module.exports = {
         jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
             if(decoded){
                 req.body.apiKey = decoded.apiKey
+                req.body.username = decoded.username
             }
             if (err) {
                 return res.status(403).json({ error: "Invalid token ❌" })
